75b7c8b9b12b9c8b08af3211f584437abec81e77
Robert Sato <rssato@ucsc.edu>
2019-10-11 15:42:33 -0700

    Base app with nothing but 3 pages

---
+++ Nutrient+/Nutrient+/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Nutrient+
+//
+//  Created by Robert Sato on 10/11/19.
+//  Copyright © 2019 Robert Sato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+}
+
