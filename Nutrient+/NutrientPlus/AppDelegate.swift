a0601e7910a3e32b2ad7cd6f1af5b97aca0b0b65
Robert Sato <rssato@ucsc.edu>
2019-10-22 17:21:46 -0700

    changed project from SwiftUI to Storyboard. Project simulation works

--- Nutrient+/NutrientPlus/AppDelegate.swift
+++ Nutrient+/NutrientPlus/AppDelegate.swift
@@ -1,9 +1,9 @@
 //
 //  AppDelegate.swift
-//  NutrientPlus
+//  Nutrient+
 //
-//  Created by hoo on 10/21/19.
-//  Copyright © 2019 hoo. All rights reserved.
+//  Created by Robert Sato on 10/22/19.
+//  Copyright © 2019 UCSC. All rights reserved.
 //
 
 import UIKit
@@ -42,7 +42,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
          application to it. This property is optional since there are legitimate
          error conditions that could cause the creation of the store to fail.
         */
-        let container = NSPersistentCloudKitContainer(name: "NutrientPlus")
+        let container = NSPersistentCloudKitContainer(name: "Nutrient_")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
                 // Replace this implementation with code to handle the error appropriately.
---
+++ Nutrient+/NutrientPlus/EditInfoVC.swift
@@ -0,0 +1,18 @@
+//
+//  EditInfoVC.swift
+//  NutrientPlus
+//
+//  Created by Robert Sato on 10/22/19.
+//  Copyright © 2019 hoo. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class EditInfoVC: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+}
