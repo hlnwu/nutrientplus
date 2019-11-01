55eee506b48b9d4c075255ed880af956026674f0
Victor <vye2@ucsc.edu>
2019-11-01 00:42:20 -0700

    Tableview cells are now tappable, nutrients are saved into an array list

--- Nutrient+/NutrientPlus/DestinationViewController.swift
+++ Nutrient+/NutrientPlus/DestinationViewController.swift
@@ -7,12 +7,9 @@
 //
 
 //import Foundation
-import UIKit
 
-struct foodInfo {
-    var foodName : String
-    var brandName : String
-}
+//Tutorial for semaphores and dispatchGroup: youtube.com/watch?v=6rJN_ECd1XM
+import UIKit
 
 class FoodCards : UITableViewCell {
     @IBOutlet weak var foodNameLabel: UILabel!
@@ -25,19 +22,96 @@ class DestinationViewController: UIViewController {
     
     var userInput : String = ""
     var foodCards: [foodInfo] = []
+    var nutrientCards: [nutrientInfo] = []
+    let dispatchGroup = DispatchGroup()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //foodTableView.delegate = self
-        //foodTableView.dataSource = self
+        foodTableView.delegate = self
+        foodTableView.dataSource = self
     }
+    
+
     
     @IBAction func enterButton(_ sender: UIButton) {
         userInput = String(editText.text!) //unwraps text
-        //DO API CALL HERE
-        
+        getFoods(userInput: userInput)
+        displayFoods()
     }
     
+    func getNutrients(foodID: Int) -> (Void){
+        dispatchGroup.enter()
+        nutrientCards = []
+        let foodIDString = String(foodID)
+        guard let urlGet = URL(string: "https://api.nal.usda.gov/fdc/v1/" + foodIDString + "?api_key=LbcbTPKWh9DPSB2aMJnlOyABZKdtFAC9J2iheb0L") else{ return }
+        print (urlGet)
+        let session = URLSession.shared
+        session.dataTask(with: urlGet) {(data, response, error) in
+            guard let data = data else { return }
+            do {
+                let nutrientDescription = try JSONDecoder().decode(NutrientDescription.self, from: data)
+                for items in (nutrientDescription.foodNutrients){
+                    let amount = items.amount
+                    let unitName = items.nutrient.unitName
+                    let nutrientName = items.nutrient.name
+                    let card = nutrientInfo(amount: amount, unitName: unitName, nutrientName: nutrientName)
+                    //print(card)
+                    self.nutrientCards.append(card)
+                }
+                self.dispatchGroup.leave()
+            } catch let jsonErr {
+                print ("Error Serializing Json: ", jsonErr)
+            }
+        }.resume()
+    }
+    
+    func getFoods(userInput: String) -> (Void){
+        dispatchGroup.enter()
+        foodCards = []
+        
+        let parameters = ["generalSearchInput":userInput]
+        guard let urlPost = URL(string: "https://api.nal.usda.gov/fdc/v1/search?api_key=LbcbTPKWh9DPSB2aMJnlOyABZKdtFAC9J2iheb0L") else{ return }
+        var request = URLRequest(url : urlPost)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        guard let httpBody = try? JSONSerialization.data(withJSONObject: parameters, options: []) else {
+            return }
+        request.httpBody = httpBody
+        
+        let session = URLSession.shared
+        session.dataTask(with: request) { (data, response, error) in
+            guard let data = data else { return }
+            
+            do {
+                let foodDescription = try JSONDecoder().decode(FoodDescription.self, from: data)
+                for items in (foodDescription.foods){
+                    let card = foodInfo(foodName: items.description, brandName: items.brandOwner ?? "N/A", foodID: items.fdcId)
+                    self.foodCards.append(card)
+                    
+                }
+                self.dispatchGroup.leave()
+            } catch let jsonErr {
+                print ("Error Serializing Json: ", jsonErr)
+            }
+        }.resume()
+    }
+    
+    func printNutrients(){
+        dispatchGroup.notify(queue: .main){
+            for i in self.nutrientCards{
+                print (i.amount, i.unitName, i.nutrientName)
+            }
+        }
+    }
+    
+    func displayFoods(){
+        dispatchGroup.notify(queue: .main){
+            for i in self.foodCards{
+                print (i.foodName, i.brandName)
+            }
+            self.foodTableView.reloadData()
+        }
+    }
 }
 
 
@@ -53,5 +127,12 @@ extension DestinationViewController: UITableViewDataSource, UITableViewDelegate{
         cell.foodNameLabel?.text = card.foodName
         cell.brandLabel?.text = card.brandName
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let card = foodCards[indexPath.row]
+        let foodID = card.foodID
+        getNutrients(foodID: foodID)
+        printNutrients()
     }
 }
--- Nutrient+/NutrientPlus/PersistenceService.swift
+++ Nutrient+/NutrientPlus/PersistenceService.swift
@@ -17,14 +17,14 @@ class PersistenceService {
         return persistentContainer.viewContext
     }
     
-    static var persistentContainer: NSPersistentCloudKitContainer = {
+    static var persistentContainer: NSPersistentContainer = {
         /*
          The persistent container for the application. This implementation
          creates and returns a container, having loaded the store for the
          application to it. This property is optional since there are legitimate
          error conditions that could cause the creation of the store to fail.
         */
-        let container = NSPersistentCloudKitContainer(name: "NutrientPlus")
+        let container = NSPersistentContainer(name: "NutrientPlus")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
                 // Replace this implementation with code to handle the error appropriately.
